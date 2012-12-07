beb63252a61733bc1df648fc6fe35b00f49523d7
gauged <damien.gauge@student.ecp.fr>
2012-12-07 13:40:53 +0100

    Update PBSproject/Shape.h
    new "nature" method and handling of collisions.

--- PBSproject/Shape.h
+++ PBSproject/Shape.h
@@ -8,11 +8,11 @@ class Shape {
 public:
 	virtual Shape *clone() {return 0;};
 	virtual void update(Shape *newSh) {};
-	virtual void setCollisionResponse(Shape *collidingSh, const float &xCollision, const float &yCollision, const int &edgeIndex, Collision *collision) {};
-	virtual void handleResponseImpulse(const float &nx, const float &ny,
-										const float &rx, const float &ry,
-										const float &impulseCoeff){};
+	virtual void setCollisionResponse(Shape *collidingSh, const int &pointIndex, const int &edgeIndex, Collision *collision) {};
+	virtual void handleResponseImpulse(const float &nx, const float &ny, const float &rx, const float &ry,
+						const std::vector<int> &cv, const float &impulseCoeff){};
 	virtual void copyTo(Shape *newShape) {};
+	virtual int nature(){return 0;};
 
 	std::vector<float> vertices;
 	int nVertices;
