13f6761e9e52d48d70a9d043a96e621afea6fd80
gauged <damien.gauge@student.ecp.fr>
2012-12-07 13:38:52 +0100

    Update PBSproject/RigidBody.h
    few changes to match new architecture.

--- PBSproject/RigidBody.h
+++ PBSproject/RigidBody.h
@@ -10,24 +10,26 @@ public:
 	virtual void setYAcc(const float &x, const float &v, float &a);
 	virtual void setAngularAcc(const float &x, const float &v, float &a);
 	virtual void update(Shape *newSh);
-	virtual void setCollisionResponse(Shape *collidingSh, const float &xCollision, const float &yCollision, const int &edgeIndex, Collision *collision);
-	virtual void handleResponseImpulse(const float &nx, const float &ny,
-										const float &rx, const float &ry,
-										const float &impulseCoeff);
+	virtual void setCollisionResponse(Shape *collidingSh, const int &pointIndex, const int &edgeIndex, Collision *collision);
+	virtual void handleResponseImpulse(const float &nx, const float &ny, const float &rx, const float &ry,
+						const std::vector<int> &cv, const float &impulseCoeff);
 	virtual void copyTo(Shape *newShape);
 	virtual RigidBody * clone() {return 0;};
+	int nature(){return 1;};
 
 	float xPos,yPos,angularPos;
 	float xVel,yVel,angularVel;
 
 	float Tx,Ty,Rx,Ry;
 
+	float e;
+	float oneOverMass;
+
 	RungeKuttaODESolver *solver;
 
 protected:
 	float mass, momentOfInertia;
-	float oneOverMass, oneOverI;
-	float e;
+	float oneOverI;
 	float gravityScale;
 	float friction;
 
