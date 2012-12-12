1319f6a55a196bf4d1d232ed0464aba1272db512
gauged <damien.gauge@student.ecp.fr>
2012-12-12 18:08:49 +0100

    Update PBSproject/DeformableObject.h
    now with impicit euler, using Eigen.

--- PBSproject/DeformableObject.h
+++ PBSproject/DeformableObject.h
@@ -1,12 +1,16 @@
 #ifndef DEFORMABLEOBJECT_H_
 #define DEFORMABLEOBJECT_H_
 #include <vector>
-#include "utilities.h"
+#include "Eigen/Dense"
 #include "Shape.h"
+using namespace std;
+
+typedef Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic> MatrixXf;
+typedef Eigen::Matrix<float, Eigen::Dynamic, 1> VectorXf;
 
 class DeformableObject : public Shape {
 public :
-	DeformableObject(vector<Vector2f> _vlist,vector<Vector3i> _flist, vector<int> _contour, float _E,float _rho, float _nu, float _alpha, float _beta, float _e);
+	DeformableObject(vector<float> _vlist,vector<int> _flist, vector<int> _contour, float _E,float _rho, float _nu, float _alpha, float _beta, float _e);
 	void update(Shape *newSh);
 	void setCollisionResponse(Shape *collidingSh, const int &pointIndex,const int &edgeIndex, Collision *collision);
 	void handleResponseImpulse(const float &nx, const float &ny,
@@ -15,20 +19,21 @@ const float &rx, const float &ry, const vector<int> &cv,const float &impulseCoef
 	int nature(){return 2;};
 	DeformableObject * clone();
 
-	vector<Vector2f> force;
-	vector<Vector2f> u;
-	vector<Vector2f> previous_u;
-	vector<Vector2f> velocity;
+	VectorXf force;
+	VectorXf u;
+	VectorXf velocity;
 	vector<int> contour;
-	Vector2f G;
+	float G [2];
 	float length;
 	float e;
 	float oneOverMass;
 protected :
-	SMatrixf K;
-	vector<Vector2f> vlist;
-	vector<Vector3i> flist;
+	MatrixXf K;
+	MatrixXf revMass;
 	vector<float> mass;
+	vector<float> vlist;
+	vector<int> flist;
+	Eigen::ColPivHouseholderQR<MatrixXf> solver;
 	float E, nu, rho, alpha, beta;
 
 };
